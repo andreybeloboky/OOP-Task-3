67614b397116f8fe7b611ec0e29a49a102ff8be6
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-09-19 21:56:36 +0300

    add comments with all methods

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -6,6 +6,13 @@ public class Person {
     private int age = 18;
     private String say;
 
+    /**
+     *
+     * @param firstName - the name person entered by the user;
+     * @param lastName - the lastName person entered by the user;
+     * @param age - the age person entered by the user;
+     * @param say - the phrase person entered by the user;
+     */
     public Person(String firstName, String lastName, int age, String say) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -13,15 +20,28 @@ public class Person {
         this.say = say;
     }
 
+    /**
+     *
+     * @param firstName - the name person entered by the user;
+     * @param lastName - the lastName person entered by the user;
+     */
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
 
+    /**
+     *
+     * @return the string where person something says
+     */
     public String say() {
         return new String(firstName + " " + lastName + " age " + age + " said: " + say);
     }
 
+    /**
+     *
+     * @return the string wherein description condition person
+     */
     public String move() {
         return new String(firstName + " " + lastName + " " + "move");
     }
