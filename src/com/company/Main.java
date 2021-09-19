dbcb3756606304d3d93e1bcd70568265eba93717
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-09-19 21:45:29 +0300

    create Person class and some variables

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter firstName");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter lastName");
+        String lastName = scanner.nextLine();
+        System.out.println("Enter age, default age - 18");
+        int age = scanner.nextInt();
     }
 }
---
+++ src/com/company/Person.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age = 18;
+    private String say;
+}
