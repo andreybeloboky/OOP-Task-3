978268e103f55cd29a6e1efc63310d07dadab555
Andrey Beloboky <andrei.beloboky@yandex.ru>
2021-09-19 23:41:49 +0300

    delete person1 method and parameter say

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,21 +12,29 @@ public class Main {
         String lastName = scanner.nextLine();
         System.out.println("Enter age, default age - 18");
         int age = scanner.nextInt();
-        System.out.println("Enter 1 - say or 2 - move, press any number to finish");
-        int number = scanner.nextInt();
-        switch (number) {
-            case 1 -> {
-                Scanner string = new Scanner(System.in);
-                System.out.println("Enter phrase");
-                String say = string.nextLine();
-                Person person = new Person(firstName, lastName, age, say);
-                System.out.println(person.say());
-            }
-            case 2 -> {
-                Person person1 = new Person(firstName, lastName);
-                System.out.println(person1.move());
-            }
-            default -> System.out.println("The end");
+        if (age > 0) {
+            System.out.println("Enter 1 - say or 2 - move, press any number to finish");
+            int number = scanner.nextInt();
+            Person person = new Person(firstName, lastName, age);
+            do {
+                switch (number) {
+                    case 1 -> {
+                        System.out.println(person.say());
+                        number = 100;
+                    }
+                    case 2 -> {
+                        System.out.println(person.move());
+                        number = 100;
+                    }
+                    default -> {
+                        System.out.println("Enter 1 - say or 2 - move, press any number to finish");
+                        number = scanner.nextInt();
+                    }
+                }
+            } while ((number >= 1 && number <= 2) || number == 100);
+            System.out.println("The end");
+        } else {
+            System.out.println("Incorrect enter. Example: Age should be more then 0");
         }
     }
 }
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -4,35 +4,23 @@ public class Person {
     private String firstName;
     private String lastName;
     private int age = 18;
-    private String say;
 
     /**
      * @param firstName - the name person entered by the user;
      * @param lastName  - the lastName person entered by the user;
      * @param age       - the age person entered by the user;
-     * @param say       - the phrase person entered by the user;
      */
-    public Person(String firstName, String lastName, int age, String say) {
+    public Person(String firstName, String lastName, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
-        this.say = say;
-    }
-
-    /**
-     * @param firstName - the name person entered by the user;
-     * @param lastName  - the lastName person entered by the user;
-     */
-    public Person(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
     }
 
     /**
      * @return the string where person something says
      */
     public String say() {
-        return new String(firstName + " " + lastName + " age " + age + " said: " + say);
+        return new String( "My name is " + firstName + " " + lastName + " my age is " + age);
     }
 
     /**
